da12c4be0d11b5da9d19a2d33b4dbe97624231ef
8457yin <91551555+8457yin@users.noreply.github.com>
2023-12-15 22:33:40 +0800

    更新 xueqiu.js

--- xueqiu.js
+++ xueqiu.js
@@ -7,3 +7,6 @@ hostname = %APPEND% *.xueqiu.com, xqimg.imedao.com, 101.201.62.20, 101.201.62.22
 
 
 [URL Rewrite]
+^https:\/\/api\.xueqiu\.com\/query\/v1\/symbol\/search\/status\.json url reject-dict
+
+^https:\/\/api\.xueqiu\.com\/v4\/statuses\/public_timeline_by_category\.json url reject-dict
