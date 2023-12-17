449b0736fdb81ca6640af4d9eb3a9516889152d1
8457yin <91551555+8457yin@users.noreply.github.com>
2023-12-17 11:44:06 +0800

    更新 huatu.js

--- huatu.js
+++ huatu.js
@@ -1,1 +1,15 @@
-#
+# 华图
+[mitm] 
+
+hostname=ns.huatu.com
+
+
+[rewrite_local]
+# 开屏广告
+^https:\/\/ns\.huatu\.com\/u\/v5\/users\/bc\/launch\?category=200100064 url reject-dict
+# 我的主页广告
+^https:\/\/ns\.huatu\.com\/u\/v5\/users\/bc\/myOperation\?category=200100064 url reject-dict
+# 主页中间小条广告
+^https:\/\/ns\.huatu\.com\/u\/v6\/users\/bc\/homeOperation\?cv=7\.3\.390&category=200100064 url reject-dict
+# 主页最上边大图广告
+^https:\/\/ns\.huatu\.com\/u\/v5\/users\/bc\/list\?category=200100064&fur=0 url reject-dict
