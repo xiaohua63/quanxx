d00d0561929b3242895d847e873a975053ef678e
8457yin <91551555+8457yin@users.noreply.github.com>
2023-12-17 14:03:59 +0800

    更新 huatuvip.js

--- huatuvip.js
+++ huatuvip.js
@@ -1,6 +1,11 @@
 /*
 小花破解
+
+[rewrite_local]
+^https:\/\/ns\.huatu\.com\/u\/v3\/member\/user url script-response-body https://raw.githubusercontent.com/8457yin/quanxx/main/huatuvip.js
 */
+
+
 var body = $response.body;
 var url = $request.url;
 var obj = JSON.parse(body);
