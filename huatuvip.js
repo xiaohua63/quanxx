f7b061fff5e342f63d66364924af39e0dfe4eb3b
8457yin <91551555+8457yin@users.noreply.github.com>
2023-12-17 13:58:58 +0800

    更新 huatuvip.js

--- huatuvip.js
+++ huatuvip.js
@@ -5,7 +5,7 @@ var body = $response.body;
 var url = $request.url;
 var obj = JSON.parse(body);
 
-const vip = '/cms/Appapi/username/username';
+const vip = '/info';
 
 if (url.indexOf(vip) != -1) {
     obj.data.giveMember= true;
