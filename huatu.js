c8a307c530ea5f1046610114a029feeb2b6aac93
8457yin <91551555+8457yin@users.noreply.github.com>
2024-01-26 16:04:52 +0800

    更新 huatu.js
    增加两个广告

--- huatu.js
+++ huatu.js
@@ -1,7 +1,7 @@
 # 华图
 [mitm] 
 
-hostname=ns.huatu.com
+hostname=ns.huatu.com,ocfapi.huatu.com
 
 
 [rewrite_local]
@@ -13,3 +13,7 @@ hostname=ns.huatu.com
 ^https:\/\/ns\.huatu\.com\/u\/v6\/users\/bc\/homeOperation\?cv=7\.3\.390&category=200100064 url reject-dict
 # 主页最上边大图广告
 ^https:\/\/ns\.huatu\.com\/u\/v5\/users\/bc\/list\?category=200100064&fur=0 url reject-dict
+#主页模考大赛图片
+^https:\/\/ns\.huatu\.com\/u\/v6\/users\/bc\/homeOperation url reject-dict
+#主页视频弹窗
+^https:\/\/ocfapi\.huatu\.com\/api\/user\/get_online_app_red_dot url reject-dict
