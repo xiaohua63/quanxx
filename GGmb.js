576bf2438d6345a9e3058d7c828dea5b3a04d4ca
xiaohua <91551555+xiaohua63@users.noreply.github.com>
2024-08-19 17:25:38 +0800

    更新 GGmb.js
    广告模板

---
+++ GGmb.js
@@ -0,0 +1,17 @@
+#!name = 开屏广告
+#!desc = 移除开屏广告。
+#!author = 小花🅥[https://github.com/xiaohua63/quanxx/main/README.md]
+#!tag = 去广告
+#!icon = https://raw.githubusercontent.com/xiaohua63/quanxx/main/app_icon/audi.png
+#!date = 2024-08-19 16:50:06
+
+[Rule]
+# 拦截HTTPDNS
+
+
+[Rewrite]
+# 开屏广告
+
+
+[Mitm]
+hostname = 
