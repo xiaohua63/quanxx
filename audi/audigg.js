7de76d1466319f8043346d7a0120f79a10c0fe68
xiaohua <91551555+xiaohua63@users.noreply.github.com>
2024-08-19 16:43:32 +0800

    更新 audigg.js
    一汽奥迪去开屏广告

---
+++ audi/audigg.js
@@ -0,0 +1,15 @@
+#!name = 一汽奥迪去除开屏广告
+#!desc = 移除开屏广告。
+#!author = 小花🅥[https://github.com/luestr/ProxyResource/blob/main/README.md]
+#!tag = 去广告
+#!icon = https://raw.githubusercontent.com/luestr/IconResource/main/App_icon/120px/AutoHome.png
+#!date = 2024-07-17 18:20:06
+
+[Rule]
+# 拦截HTTPDNS
+
+
+[Rewrite]
+
+[Mitm]
+hostname = 
