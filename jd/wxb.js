adf4fedad057ced5310c853d8887d54161f01b43
xiaohua <91551555+xiaohua63@users.noreply.github.com>
2024-09-06 16:12:34 +0800

    更新 wxb.js
    更新

--- jd/wxb.js
+++ jd/wxb.js
@@ -1,4 +1,17 @@
+#!name = 无线宝去广告
+#!desc = 移除主页内广告。
+#!author = 小花🅥[https://github.com/xiaohua63/quanxx/main/README.md]
+#!tag = 去广告
+#!icon = https://raw.githubusercontent.com/xiaohua63/quanxx/main/app_icon/audi.png
+#!date = 2024-08-19 16:50:06
+
+[Rule]
+# 拦截HTTPDNS
+
+
 [Rewrite]
+# 主页内广告
 ^https:\/\/router-app-api\.jdcloud\.com\/v1\/regions\/cn-north-1\/unifierQuery reject-dict
+
 [mitm] 
 hostname = router-app-api.jdcloud.com
